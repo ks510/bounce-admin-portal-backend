b5c1febafed0a9e168e3c4125ef955b71b49cef8
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-20 17:47:07 +0100

    Removed unused variable in createCustomer.js

--- createCustomer.js
+++ createCustomer.js
@@ -2,7 +2,7 @@ import stripePackage from "stripe";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-  const { email, source } = JSON.parse(event.body);
+  const { email } = JSON.parse(event.body);
 
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
