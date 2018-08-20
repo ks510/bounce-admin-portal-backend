42fb8931e24809e8a28372adc393e641d3d3443a
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-20 16:23:07 +0100

    Added create product and plan API for testing purposes and mock events

--- createPlan.js
+++ createPlan.js
@@ -2,17 +2,20 @@ import stripePackage from "stripe";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-  const { name, type } = JSON.parse(event.body);
+  const { product, nickname, currency, interval, amount } = JSON.parse(event.body);
 
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
 
   try {
-    const product = await stripe.products.create({
-      name,
-      type,
+    const plan = await stripe.plans.create({
+      product,
+      nickname,
+      currency,
+      interval,
+      amount,
     });
-    console.log(product);
+    console.log(plan);
     callback(null, success({ status: true }));
   } catch (e) {
     callback(null, failure({ message: e.message }));
