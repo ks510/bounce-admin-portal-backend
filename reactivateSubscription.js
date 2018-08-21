d8e4fe8ca127fce84acd1f588e2d946760ba04a8
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-21 17:25:08 +0100

    Added new API for reactivating stripe subscriptions before end of billing period

---
+++ reactivateSubscription.js
@@ -0,0 +1,22 @@
+import stripePackage from "stripe";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const { subscription } = JSON.parse(event.body);
+
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
+
+  try {
+    await stripe.subscriptions.update(subscription, {
+      cancel_at_period_end: true
+    });
+
+    // return newly created customer ID
+    callback(null, success({ reactivate_subscription: subscription }));
+  } catch (e) {
+    callback(null, failure({ message: e.message }));
+  }
+
+
+}
