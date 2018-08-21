febacd29296867acf4c4e4d2fc6ec560c5cf29b5
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-21 15:13:17 +0100

    Added getCustomer endpoint to serverless.yml

--- getCustomer.js
+++ getCustomer.js
@@ -13,7 +13,12 @@ export async function main(event, context, callback) {
       customer
     );
 
-    callback(null, success({ stripeCustomer }));
+    if (stripeCustomer) {
+      // Return the retrieved csutomer
+      callback(null, success(stripeCustomer));
+    } else {
+      callback(null, failure({ status: false, error: "Customer not found." }));
+    }
   } catch (e) {
     callback(null, failure({ message: e.message }));
   }
