9a417f27c5682e35d2d6ed6c741c092309fa39bf
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-21 13:44:37 +0100

    Removed test API

--- testtest.js
+++
@@ -1,7 +0,0 @@
-import stripePackage from "stripe";
-
-export async function main(event, context, callback) {
-
-  const stripe = stripePackage(process.env.stripeSecretKey);
-  console.log("new test api success");
-}
