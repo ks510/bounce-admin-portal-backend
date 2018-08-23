e5cf90b2ae0c569876ce90bb15f688888d0f7b24
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-23 20:55:51 +0100

    Removed redundant resources

--- libs/dynamodb-lib.js
+++
@@ -1,9 +0,0 @@
-import AWS from "aws-sdk";
-
-// region no longer needs to be hard coded
-
-export function call(action, params) {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  return dynamoDb[action](params).promise();
-}
